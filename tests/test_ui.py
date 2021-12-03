9331f5fe6cfce62803a03c0b910f20ab74aab02d
Jamison Homatas <jhomatas48@gmail.com>
2021-12-03 04:22:11 -0600

    finished

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -9,6 +9,7 @@ from main import app
 from tests.credentials import credentials
 import base64
 
+app.config['TESTING'] = True
 client = app.test_client()
 
 creds = credentials()
@@ -32,66 +33,4 @@ def test_login():
     url = '/login'
     response = client.get(url)
     assert response.status_code == 200
-    response_post = client.post(url)
-    assert response_post.status_code == 200
 
-
-
-'''
-def test_post_route__success():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/post/test'
-
-    mock_request_headers = {
-        'authorization-sha256': '123'
-    }
-
-    mock_request_data = {
-        'request_id': '123',
-        'payload': {
-            'py': 'pi',
-            'java': 'script'
-        }
-    }
-
-    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert response.status_code == 200
-
-
-def test_post_route__failure__unauthorized():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/post/test'
-
-    mock_request_headers = {}
-
-    mock_request_data = {
-        'request_id': '123',
-        'payload': {
-            'py': 'pi',
-            'java': 'script'
-        }
-    }
-
-    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert response.status_code == 401
-
-
-def test_post_route__failure__bad_request():
-    app = Flask(__name__)
-    configure_routes(app)
-    client = app.test_client()
-    url = '/post/test'
-
-    mock_request_headers = {
-        'authorization-sha256': '123'
-    }
-
-    mock_request_data = {}
-
-    response = client.post(url, data=json.dumps(mock_request_data), headers=mock_request_headers)
-    assert response.status_code == 400
-'''
