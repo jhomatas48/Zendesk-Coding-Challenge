95f8e4d6fc83bcd6fd546d7bd7eb0d044d8af132
Jamison Homatas <jhomatas48@gmail.com>
2021-12-03 04:25:01 -0600

    added credentials.py

---
+++ tests/credentials.py
@@ -0,0 +1,10 @@
+'''
+This file is for testing only.
+Enter in your email, password, and Zendesk subdomain to test
+for specific credentials.
+'''
+def credentials():
+    return {"email": "your@email.com",
+            "password": "your_password",
+            "subdomain": "your_subdomain",
+            "name": "your_full_name"}
