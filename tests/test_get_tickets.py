005755224472e5f94bc31df3132752fa0a81ea8a
Jamison Homatas <jhomatas48@gmail.com>
2021-12-02 23:58:57 -0600

    added test ticket

--- tests/test_get_tickets.py
+++ tests/test_get_tickets.py
@@ -4,7 +4,59 @@ from tickets.ticket import Ticket
 from tickets.get_tickets import get_tickets, get_name_and_email, format_tickets
 from tickets.get_tickets import get_tickets
 
+def test_ticket_class():
+    data = json.loads({
+            "url": "https://zccvanderbilt.zendesk.com/api/v2/tickets/1.json",
+            "id": 1,
+            "external_id": null,
+            "via": {
+                "channel": "sample_ticket",
+                "source": {
+                    "from": {},
+                    "to": {},
+                    "rel": null
+                }
+            },
+            "created_at": "2021-11-24T21:48:54Z",
+            "updated_at": "2021-11-24T21:48:54Z",
+            "type": "incident",
+            "subject": "Sample ticket: Meet the ticket",
+            "raw_subject": "Sample ticket: Meet the ticket",
+            "description": "Hi there,\n\nI’m sending an email because I’m having a problem setting up your new product. Can you help me troubleshoot?\n\nThanks,\n The Customer\n\n",
+            "priority": "normal",
+            "status": "open",
+            "recipient": null,
+            "requester_id": 421988152152,
+            "submitter_id": 421988150252,
+            "assignee_id": 421988150252,
+            "organization_id": null,
+            "group_id": 4411237682452,
+            "collaborator_ids": [],
+            "follower_ids": [],
+            "email_cc_ids": [],
+            "forum_topic_id": null,
+            "problem_id": null,
+            "has_incidents": false,
+            "is_public": true,
+            "due_at": null,
+            "tags": [
+                "sample",
+                "support",
+                "zendesk"
+            ],
+            "custom_fields": [],
+            "satisfaction_rating": null,
+            "sharing_agreement_ids": [],
+            "fields": [],
+            "followup_ids": [],
+            "ticket_form_id": 360003523752,
+            "brand_id": 360007072512,
+            "allow_channelback": false,
+            "allow_attachments": true
+        })
 
+
+'''
 def test_retrieved_data():
     with open("tests/tickets.json") as tickets_json:
         input_tickets = json.loads(tickets_json.read())
@@ -26,3 +78,4 @@ def test_retrieved_data():
 if __name__ == '__main__':
     test_retrieved_data()
     print(0)
+'''
